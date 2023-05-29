791d0797276c03d9c9f807efa023d79860347bf6
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-05-29 00:21:07 -0300

    arumando projetos carousel

--- index.php
+++ index.php
@@ -3,15 +3,29 @@
     include_once("templates/header.php")
 
 ?>
-
+<script defer src="js/proj.js"></script>
 <div class="main-content">
 
     <div class="apre">
         <h1>Meu nome é Gustavo</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio atque itaque voluptatem labore minima optio et iusto laboriosam facere quos. Dolorum consequatur eius mollitia minus velit debitis nam excepturi minima?</p>
-        <div class="proj">
 
-        </div>
+        <div class="proj">
+            <h1>Projetos</h1>
+            <div class="container">
+  <button class="arrow-left control" aria-label="Previous image">◀</button>
+  <button class="arrow-right control" aria-label="Next Image">▶</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+            <img src="https://source.unsplash.com/random/250x250/?beach" alt="Beach Image" class="item current-item">
+            <img src="https://source.unsplash.com/random/250x250/?animal" alt="Animal Image" class="item current-item">
+            <img src="https://source.unsplash.com/random/250x250/?street" alt="Street Image" class="item current-item">
+            <img src="https://source.unsplash.com/random/250x250/?zoo" alt="Zoo Image" class="item current-item">
+            <img src="https://source.unsplash.com/random/250x250/?model" alt="Model Image" class="item current-item">
+            </div>
+           </div>
+         </div> <!-- container -->
+       </div><!-- PROJ -->
     </div>
 
     <div class="skills">
